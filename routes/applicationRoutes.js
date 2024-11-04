d634f229e83ce170ff73dbf4ee106a347d21848f
Aditya Khare <adityakhare863@gmail.com>
2024-11-05 02:57:20 +0530

    added authentication middleware to application routes

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -4,8 +4,8 @@ const applicationController = require('../controllers/applicationController');
 const { authenticateToken } = require('../middlewares/auth');
 
 // Application routes
-router.post('/', applicationController.createApplication);                    // Submit a candidate application
-router.get('/:jobId', applicationController.getApplicationsByJob);            // Retrieve all applications for a job
-router.get('/filter', applicationController.filterApplications);              // Filter applications by candidate name, status, or experience
+router.post('/', authenticateToken, applicationController.createApplication);                    // Submit a candidate application
+router.get('/:jobId' , applicationController.getApplicationsByJob);            // Retrieve all applications for a job
+router.get('/filter', authenticateToken , applicationController.filterApplications);              // Filter applications by candidate name, status, or experience
 
 module.exports = router;
