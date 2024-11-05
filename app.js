5af96309dbe87aa0bd1f6f923aace3e840e5f785
Aditya Khare <adityakhare863@gmail.com>
2024-11-05 09:54:17 +0530

    added authentication routes to app.js

--- app.js
+++ app.js
@@ -2,18 +2,19 @@ const express = require('express');
 const connectDB = require('./config/database');
 const jobRoutes = require('./routes/jobRoutes');
 const applicationRoutes = require('./routes/applicationRoutes');
+const authRoutes = require('./routes/authRoutes'); // Import auth routes
 const app = express();
 require('dotenv').config();
 
-// Middleware
 app.use(express.json());
 
 // Connect to MongoDB
 connectDB();
 
 // Routes
-app.use('/api/jobs', jobRoutes);
-app.use('/api/applications', applicationRoutes);
+app.use('/api/auth', authRoutes); // Authentication routes
+app.use('/api/jobs', jobRoutes);  // Job routes
+app.use('/api/applications', applicationRoutes); // Application routes
 
 // Start Server
 const PORT = process.env.PORT || 5000;
