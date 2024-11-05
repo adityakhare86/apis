3796294ac14f7eb7eeb66b662b6208408c38db1d
Aditya Khare <adityakhare863@gmail.com>
2024-11-05 10:58:34 +0530

    stripping bearer from auth token in authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,16 @@
 const jwt = require('jsonwebtoken');
+
 exports.authenticateToken = (req, res, next) => {
+  // Get token from the Authorization header
   const token = req.header('Authorization');
   if (!token) return res.status(401).json({ error: 'Access denied' });
+
+  // Strip the 'Bearer ' prefix if it exists
+  const bearerToken = token.startsWith('Bearer ') ? token.slice(7) : token;
+
   try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    // Verify the token
+    const verified = jwt.verify(bearerToken, process.env.JWT_SECRET);
     req.user = verified;
     next();
   } catch (error) {
